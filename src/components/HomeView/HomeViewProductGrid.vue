73e82787eaf1d27445df2d293fbbce20b6ae2080
Daria Kurmaeva <daria.kurmaeva@gmail.com>
2025-04-18 19:19:02 +0600

    feat: Add ProductGrid component

---
+++ src/components/HomeView/HomeViewProductGrid.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="product-grid">
+    <div v-for="(product, index) in props.products" :key="index">{{ product }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{ products: [] }>();
+</script>
+
+<style src="./HomeViewProductGrid.css" scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,11 @@
 <template>
-  <main>
-    Hello
-  </main>
+  <HomeViewProductGrid :products="products" />
 </template>
+
+<script setup lang="ts">
+import HomeViewProductGrid from '@/components/HomeView/HomeViewProductGrid.vue'
+import { ref } from 'vue';
+const products = ref(new Array(20).fill('Product'));
+</script>
+
+<style scoped></style>
